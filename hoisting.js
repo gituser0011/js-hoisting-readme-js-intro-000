14ff071374068602d444f3b1c23e60e5aa7e8c94
S  1 <lokohopo@gmail.com>
2020-11-16 22:23:29 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,9 +3,10 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  var callMe = "maybe";
-  return callMe;
+  var lyric = "maybe";
+  return lyric;
   }
+
 
   function crazy() {
   // fix the code in here:
